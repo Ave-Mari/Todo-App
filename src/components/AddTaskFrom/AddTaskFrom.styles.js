da764aecc4b4c22e109f73af54f8912fa58de449
Mari Vaypan <avemari98@gmail.com>
2023-06-13 16:42:02 +0300

    add styled component to form

---
+++ src/components/AddTaskFrom/AddTaskFrom.styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+
+        display: flex;
+        justify-content: center;
+        padding-bottom: 13px;
+
+        .form {
+            display: flex;
+            justify-content: space-between;
+            width: 680px;
+            align-items: center;
+          }
+          .add-task-input {
+            width: 100%;  
+            border: none;
+            padding: 11px;
+            border-radius: 5px;
+            background: rgb(205, 225, 243);
+          }
+          
+          .input-block {
+              width: 77%;
+            display: flex;
+            align-items: center;
+          }
+`
