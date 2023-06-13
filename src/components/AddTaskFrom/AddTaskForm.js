01625ad0ebf615fbcd70b9768352fef6c851f8f7
Mari Vaypan <avemari98@gmail.com>
2023-06-13 16:49:47 +0300

    fix form view

--- src/components/AddTaskFrom/AddTaskForm.js
+++ src/components/AddTaskFrom/AddTaskForm.js
@@ -6,10 +6,9 @@ export default function AddTaskForm( {handleChange, handleSubmit, todo} ) {
     <Wrapper>
 <form onSubmit={handleSubmit} className="form">
               <div className="input-block">
-              <label htmlFor="task" className="task-label">Task</label>
-              <input type="text" id="task" name="task" value={todo.task} onChange={handleChange} className='add-task-input'/>
+              <input placeholder='Your task to do' type="text" id="task" name="task" value={todo.task} onChange={handleChange} className='add-task-input'/>
               </div>             
-              <button type="submit" className="btn-add-task">Add Task</button>
+              <button type="submit" className="btn-add-task">Add task</button>
     </form>
 
     </Wrapper>
--- src/components/AddTaskFrom/AddTaskFrom.styles.js
+++ src/components/AddTaskFrom/AddTaskFrom.styles.js
@@ -9,15 +9,18 @@ export const Wrapper = styled.section`
         .form {
             display: flex;
             justify-content: space-between;
-            width: 680px;
+            width: 550px;
             align-items: center;
           }
           .add-task-input {
             width: 100%;  
             border: none;
-            padding: 11px;
+            padding: 11px 13px;
             border-radius: 5px;
             background: rgb(205, 225, 243);
+          }
+          .add-task-input[placeholder] {
+            font-size: 21px;
           }
           
           .input-block {
