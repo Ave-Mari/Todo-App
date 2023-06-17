d260ae02512213079ee26cafad3ec5eb7cff7dc9
Mari Vaypan <avemari98@gmail.com>
2023-06-17 23:26:15 +0300

    create filter buttons

---
+++ src/components/FilterButtons/FilterButtons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Wrapper } from './FilterButtons.styles'
+
+export default function FilterButtons({showAll, showActive, showCompleted}) {
+
+   
+
+  return (
+    <Wrapper>
+        <button onClick={showAll}>All</button>
+        <button onClick={showActive}>Active</button>
+        <button onClick={showCompleted}>Completed</button>
+    </Wrapper>
+  )
+}
