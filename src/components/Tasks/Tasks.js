c21c80082fa2daf46288679f05b528a3dbeb56d9
Mari Vaypan <avemari98@gmail.com>
2023-06-17 23:58:27 +0300

    add condition

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -16,51 +16,55 @@ export default function Tasks( {
                                 } ) {
 
   return (
-    <Wrapper>
-         {sortedTasks.length == 0 && (
-                  <p className="no-task">There is no task...</p>
-                )}
-                {sortedTasks.sort(sortTodos).map((item) => {
-                  const {id, task, completed} = item;
-                  return (
-                    <ul 
-                      key={id} 
-                      draggable={true} 
-                      className="todo-list"
-                      onDragStart={(e) => dragStartHandler(e, item)}
-                      onDragLeave={(e) => dragEndHandler(e)}
-                      onDragEnd={(e) => dragEndHandler(e)}
-                      onDragOver={(e) => dragOverHandler(e)}
-                      onDrop={(e) => dropHandler(e, item)}
-                    >
-                      <li className="todo-item" >
-                        <p className={completed ? 'task-checked' : 'task-unchecked'}>
-                          {task}
-                          </p> 
-                          <div className="todo-checkbox-block">
-                            <label className="todo-checkbox-label">
-                          <input 
-                          type='checkbox' 
-                          className="todo-checkbox"
-                          checked={checked[id]}
-                          onChange={(e) => {taskChecked(id)}}
-                       />
-                       </label>
-                          </div>
-                       
-                      <button className="todo-item-btn" onClick={() => 
-                      setTimeout(() => {
-                        removeItem(id)
-                      }, 800)
-                        
-                        }>
-                          🗑️
-                          </button>
-                      </li>
-                      
-                    </ul>
-                  )
-                })}
-    </Wrapper>
+    <>
+     {sortedTasks.length == 0 ? ( <p className="no-task">There is no task...</p>) : 
+     <Wrapper>
+        
+        {sortedTasks.sort(sortTodos).map((item) => {
+          const {id, task, completed} = item;
+          return (
+            <ul 
+              key={id} 
+              draggable={true} 
+              className="todo-list"
+              onDragStart={(e) => dragStartHandler(e, item)}
+              onDragLeave={(e) => dragEndHandler(e)}
+              onDragEnd={(e) => dragEndHandler(e)}
+              onDragOver={(e) => dragOverHandler(e)}
+              onDrop={(e) => dropHandler(e, item)}
+            >
+              <li className="todo-item" >
+                <p className={completed ? 'task-checked' : 'task-unchecked'}>
+                  {task}
+                  </p> 
+                  <div className="todo-checkbox-block">
+                    <label className="todo-checkbox-label">
+                  <input 
+                  type='checkbox' 
+                  className="todo-checkbox"
+                  checked={checked[id]}
+                  onChange={(e) => {taskChecked(id)}}
+               />
+               </label>
+                  </div>
+               
+              <button className="todo-item-btn" onClick={() => 
+              setTimeout(() => {
+                removeItem(id)
+              }, 800)
+                
+                }>
+                  🗑️
+                  </button>
+              </li>
+              
+            </ul>
+          )
+        })}
+</Wrapper>
+  }
+                 
+    
+    </>
   )
 }
